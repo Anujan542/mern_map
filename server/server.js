e006282f697c0564d947794461d07fa54102cd7f
anujan542 <anujan9300@gmail.com>
2021-06-29 08:29:21 +0530

    Final deploy

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
 const db = require("./config/db");
+const path = require("path");
 
 dotenv.config();
 db();
@@ -9,12 +10,19 @@ db();
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("hello");
-});
-
 app.use("/api/user", require("./routes/userRoutes"));
 app.use("/api/pin", require("./routes/pinRoutes"));
+
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running....");
+  });
+}
 
 const port = process.env.PORT || 5000;
 
